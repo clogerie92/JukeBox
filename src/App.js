f6ddb7aafe8086a3645f6a43e31823a539d5705a
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-27 13:07:50 -0400

    added Navbar component to App.js with routes to corresponding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import LandingPage from '.Components/Pages/LandingPage';
+import Home from '.Components/Pages/Home';
+import Dashboard from '.Components/Pages/Dashboard';
+import Signin from '.Components/Pages/Signin';
+import Signup from '.Components/Pages/Signup';
+import Shop from '.Components/Pages/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <h1>JukeBox</h1>
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/shop" element={<Shop />}/>
+        </Routes>
+      </Router>
+
   );
 }
 
