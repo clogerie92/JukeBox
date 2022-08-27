b4a4cb5145c5a3d72f8fb0c4daea1950b279192b
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-27 13:55:41 -0400

    added content to bio-container in landing page, added contact form in landing page, added functionality to send emails, added mobile responsiveness to navbar and landingpage, changed color of navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import LandingPage from '.Components/Pages/LandingPage';
-import Home from '.Components/Pages/Home';
-import Dashboard from '.Components/Pages/Dashboard';
-import Signin from '.Components/Pages/Signin';
-import Signup from '.Components/Pages/Signup';
-import Shop from '.Components/Pages/Shop';
+import LandingPage from './Components/Pages/LandingPage';
+import Home from './Components/Pages/Home';
+import Dashboard from './Components/Pages/Dashboard';
+import Signin from './Components/Pages/Signin';
+import Signup from './Components/Pages/Signup';
+import Shop from './Components/Pages/Shop';
+import Footer from './Components/Footer';
 
 function App() {
   return (
       <Router>
         <Navbar />
-        <Routes>
+         <Routes>
           <Route path="/" element={<LandingPage />}/>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/signin" element={<Signin />}/>
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="/shop" element={<Shop />}/>
+          {/* <Route path="/home" element={<Home />}/> */}
+          {/* <Route path="/dashboard" element={<Dashboard />}/> */}
+          {/* <Route path="/signin" element={<Signin />}/> */}
+          {/* <Route path="/signup" element={<Signup />}/> */}
+          {/* <Route path="/shop" element={<Shop />}/> */}
         </Routes>
+        <Footer />
       </Router>
-
   );
 }
 
---
+++ src/Components/Pages/LandingPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "../Styles/LandingPage.css";
+import { validateEmail } from "../../utils/helpers";
+
+const LandingPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    console.log(inputType, target.id);
+
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+      console.log(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Please enter a valid email.");
+      return;
+    } else if (!name || !message) {
+      setErrorMessage("Please fill in all fields.");
+      return;
+    } else {
+      setErrorMessage("");
+    }
+
+    alert(`Thank you for submitting your message, ${name}`);
+    setName("");
+    setEmail("");
+    setMessage("");
+    window.open(`mailto:${email}`);
+  };
+    return (
+        <div className="landing-container">
+                <p className="bio-container">
+                    Hi! My name is Carl Logerie and I am a full stack software engineer. I recently graduated from the Rutgers Coding Bootcamp and wanted to test my skills by building a web application from scratch. As a music lover, I wanted to build a web app that curates a playlist and tour dates of an artist that was searched. This web app also features a record store where users can purchase vinyl records. If you have any questions about this web application or want to get in contact feel free to send me a message below!
+                </p>
+            <div className="contact-container">
+                <form onSubmit={handleFormSubmit}>
+                    <input placeholder="Name" type="text" name="name" value={name} onChange={handleInputChange} />
+                    <input placeholder="Email" type="text" name="email" value={email} onChange={handleInputChange}  />
+                    <textarea  placeholder="Message" name="message" id="" cols="30" rows="10" value={message} onChange={handleInputChange}></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+            {errorMessage && (
+                <div className="error-message">
+                  <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default LandingPage;
